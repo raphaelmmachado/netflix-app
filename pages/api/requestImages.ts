ca0c80230405490ddccf6e2d37ef6b378970c7ce
Raphael Machado <raphael.mm.91@gmail.com>
2023-01-09 03:06:13 -0300

    consertei erro que estava impedindo deploy

--- pages/api/requestImages.ts
+++ pages/api/requestImages.ts
@@ -1,20 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { dinamicRequests } from "../../utils/requests";
-interface Req {
-  data: {};
-}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Req>
-) {
-  if (req.method === "POST") {
-    // Process a POST request
-    const id = req.body.id;
-    const { fecthIndividualMovie } = dinamicRequests(id);
-    const response = await fetch(fecthIndividualMovie);
-    const data = await response.json();
-    res.status(200).json({ data: data });
-  } else {
-    // Handle any other HTTP method
-  }
-}
+  res: NextApiResponse
+) {}
