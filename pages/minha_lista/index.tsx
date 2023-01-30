66fcb9db629cc78d6566a501acb42f13f0f29040
Raphael Machado <raphael.mm.91@gmail.com>
2023-01-30 17:17:28 -0300

    Update index.tsx

--- pages/minha_lista/index.tsx
+++ pages/minha_lista/index.tsx
@@ -3,12 +3,14 @@ import { useContext } from "react";
 import { Context } from "../../context/ContextProvider";
 import ListComponent from "../../components/minha_lista/ListComponent";
 import Loading from "../../components/auth/Loading";
+import Header from "../../components/header/Header";
 
 export default function App() {
   const { myList } = useContext(Context);
 
   return (
     <>
+      <Header className="bg-transparent" />
       {myList && myList.length > 0 ? (
         <ListComponent movies={myList} title="Minha Lista" />
       ) : (
